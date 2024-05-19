33f054018b3f9202f0b7700c5e31f81c8e17e313
Swizec Teller <swizec@swizec.com>
2024-05-18 20:25:23 -0700

    Setup joyui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@fontsource/inter";
+import { CssVarsProvider } from "@mui/joy/styles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <CssVarsProvider>
+            <CssBaseline />
+
+            <App />
+        </CssVarsProvider>
+    </React.StrictMode>
+);
