193dd2d1a93a5b5e26f22898dbcda52f3b1cd2e0
Swizec Teller <swizec@swizec.com>
2024-05-18 21:31:31 -0700

    Enable retries

--- src/WikipediaBirthdays.tsx
+++ src/WikipediaBirthdays.tsx
@@ -5,8 +5,14 @@ import Stack from "@mui/joy/Stack";
 import CakeIcon from "@mui/icons-material/Cake";
 import Table from "@mui/joy/Table";
 import Link from "@mui/joy/Link";
+import Alert from "@mui/joy/Alert";
 import CircularProgress from "@mui/joy/CircularProgress";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { ErrorBoundary, useErrorBoundary } from "react-error-boundary";
+import { useSuspenseQuery, useQueryClient } from "@tanstack/react-query";
+
+// Set to true to simulate an error when fetching data
+// it will succeed on retry
+let FAIL_LOADING = false;
 
 type BirthEntry = {
     pages: Page[];
@@ -29,11 +35,20 @@ function useBirths(day: Date) {
     const query = useSuspenseQuery<BirthEntry[]>({
         queryKey: ["wikipedia-birthdays", day],
         queryFn: async () => {
+            if (FAIL_LOADING) {
+                throw new Error("Simulated error!");
+            }
+
             const response = await fetch(
                 `https://en.wikipedia.org/api/rest_v1/feed/onthisday/births/${
                     day.getMonth() + 1
                 }/${day.getDate()}`
             );
+
+            if (!response.ok) {
+                throw new Error("Error fetching births");
+            }
+
             const { births } = (await response.json()) as {
                 births: BirthEntry[];
             };
@@ -63,11 +78,17 @@ const Birthdays: FC<{ day: Date }> = ({ day }) => {
                     // we can use index as key because the list is static
                     <tr key={index}>
                         <td>
-                            <Link
-                                href={birth.pages[0].content_urls.desktop.page}
-                            >
-                                {birth.text.split(",")[0]}
-                            </Link>
+                            {birth.pages[0] ? (
+                                <Link
+                                    href={
+                                        birth.pages[0].content_urls.desktop.page
+                                    }
+                                >
+                                    {birth.text.split(",")[0]}
+                                </Link>
+                            ) : (
+                                birth.text.split(",")[0]
+                            )}
                         </td>
                         <td>{birth.text.split(",")[1]}</td>
                         <td>{birth.year}</td>
@@ -78,18 +99,40 @@ const Birthdays: FC<{ day: Date }> = ({ day }) => {
     );
 };
 
+const LoadError: FC<{ day: Date }> = ({ day }) => {
+    const queryClient = useQueryClient();
+    const { resetBoundary } = useErrorBoundary();
+
+    function refetch() {
+        FAIL_LOADING = false;
+        resetBoundary();
+        queryClient.refetchQueries({
+            queryKey: ["wikipedia-birthdays", day],
+        });
+    }
+
+    return (
+        <Alert color="danger">
+            Error fetching births for {getPrettyDate(day)}{" "}
+            <Button onClick={refetch}>Retry</Button>
+        </Alert>
+    );
+};
+
+function getPrettyDate(date: Date) {
+    return date.toLocaleDateString(undefined, {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+    });
+}
+
 export const WikipediaBirthdays: FC<{ day: Date }> = ({ day }) => {
     const [showBirthdays, setShowBirthdays] = useState(false);
 
     return (
         <Stack alignItems="center" spacing={2}>
-            <Typography level="h2">
-                {day.toLocaleDateString(undefined, {
-                    weekday: "long",
-                    month: "long",
-                    day: "numeric",
-                })}
-            </Typography>
+            <Typography level="h2">{getPrettyDate(day)}</Typography>
 
             <Button
                 startDecorator={<CakeIcon />}
@@ -100,7 +143,9 @@ export const WikipediaBirthdays: FC<{ day: Date }> = ({ day }) => {
 
             {showBirthdays ? (
                 <Suspense fallback={<CircularProgress />}>
-                    <Birthdays day={day} />
+                    <ErrorBoundary fallback={<LoadError day={day} />}>
+                        <Birthdays day={day} />
+                    </ErrorBoundary>
                 </Suspense>
             ) : null}
         </Stack>
